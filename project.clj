88b629fe36fcc2be024847c080d9560252e16868
miloas <0x9357@gmail.com>
2015-08-10 20:19:57 +0800

    add project

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject lrun "0.1.0"
+  :description "Run process in limit resources."
+  :url "http://github.com"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.flatland/useful "0.11.3"])
---
+++ test/lrun/core_test.clj
@@ -0,0 +1,7 @@
+(ns lrun.core-test
+  (:require [clojure.test :refer :all]
+            [lrun.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
